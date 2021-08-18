4c81d6afd29c10d2a09c4efc1a6ff8b5eb0904c9
lvshclt <lvshclt@gmail.com>
2021-08-18 14:37:09 +0900

    2-6 격자판 최대합
    세로로 탐색하려면 이중for문 불가피한듯
    인덱스 0 부터 인덱스 n-1 이므로,
    arr[i][n-1-i]하면 역순이다

--- js.js
+++ js.js
@@ -355,8 +355,108 @@
   console.log(solution(arr));
 })();
 
-//2-1 큰 수 출력하기
-(() => {})();
+//2-6 격자판 최대합
+(() => {
+  function solution(arr) {
+    let answer = Number.MIN_SAFE_INTEGER;
+    let n = arr.length;
+    let sum1 = (sum2 = 0);
+    for (let i = 0; i < n; i++) {
+      sum1 = sum2 = 0;
+      for (let j = 0; j < n; j++) {
+        sum1 += arr[i][j];
+        sum2 += arr[j][i];
+      }
+      answer = Math.max(answer, sum1, sum2);
+    }
+    sum1 = sum2 = 0;
+    for (let i = 0; i < n; i++) {
+      sum1 += arr[i][i];
+      sum2 += arr[i][n - i - 1];
+    }
+    answer = Math.max(answer, sum1, sum2);
+    return answer;
+  }
+
+  let arr = [
+    [10, 13, 10, 12, 15],
+    [12, 39, 30, 23, 11],
+    [11, 25, 50, 53, 15],
+    [19, 27, 29, 37, 27],
+    [19, 13, 30, 13, 19],
+  ];
+  console.log(solution(arr));
+})();
+(() => {
+  function solution(arr) {
+    let answer = Number.MIN_SAFE_INTEGER;
+    let n = arr.length;
+    arr.forEach((v) => {
+      if (answer < v.reduce((a, c) => a + c, 0))
+        answer = v.reduce((a, c) => a + c, 0);
+    });
+
+    let d1 = 0;
+    let d2 = 0;
+    arr.forEach((v, i) => {
+      d1 += v[i];
+      d2 += v[n - 1 - i];
+    });
+    // console.log(d1, d2);
+    let d3;
+    for (let i = 0; i < n; i++) {
+      d3 = 0;
+      for (let j = 0; j < n; j++) {
+        d3 += arr[j][i];
+      }
+      // console.log(d3);
+    }
+    answer = Math.max(answer, d1, d2, d3);
+    return answer;
+  }
+
+  let arr = [
+    [10, 13, 10, 12, 15],
+    [12, 39, 30, 23, 11],
+    [11, 25, 50, 53, 15],
+    [19, 27, 29, 37, 27],
+    [19, 13, 30, 13, 19],
+  ];
+  console.log(solution(arr));
+})();
+(() => {
+  function solution(arr) {
+    let answer = Number.MIN_SAFE_INTEGER;
+    let n = arr.length;
+    let sum1 = (sum2 = 0);
+
+    for (let i = 0; i < n; i++) {
+      sum1 = sum2 = 0;
+      for (let j = 0; j < n; j++) {
+        sum1 += arr[i][j];
+        sum2 += arr[j][i];
+      }
+      answer = Math.max(answer, sum1, sum2);
+    }
+
+    sum1 = sum2 = 0;
+    for (let i = 0; i < n; i++) {
+      sum1 += arr[i][i];
+      sum2 += arr[i][n - 1 - i];
+    }
+    answer = Math.max(answer, sum1, sum2);
+    return answer;
+  }
+
+  let arr = [
+    [10, 13, 10, 12, 15],
+    [12, 39, 30, 23, 11],
+    [11, 25, 50, 53, 15],
+    [19, 27, 29, 37, 27],
+    [19, 13, 30, 13, 19],
+  ];
+  console.log(solution(arr));
+})();
 
 //2-1 큰 수 출력하기
 (() => {})();
